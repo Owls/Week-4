c1369f141ccb3ca7e1a0bc701952ff9b020edccc
Hunter <hunter@Nest.local>
2021-02-10 17:23:23 -0500

    GM Final

--- app1.js
+++ app1.js
@@ -2,6 +2,8 @@
  * Console Methods
  * Provides different handy features for debugging
  * Reminds me of Includes? 
+ * These two Methods print an Error and a Warning to the Console. 
+ * CHECK: Functioning as intended.
  */
 console.warn("Uh oh, warning time!");
 console.error("Well, this error probably isn't good.");
@@ -13,33 +15,59 @@ console.error("Well, this error probably isn't good.");
  * We use var to declare a variable, which is in this case, "greeting".
  * Using console.log, we can display the instructions contained in our var, in the browser's debug console.
  * NOTE: You can save work by remembering to declare and define the var at the same time. 
- * REPORT: Functioning as intended. 
+ * CHECK: Functioning as intended. 
  */
-
 var greeting = "Hello, Idiot."
 console.log(greeting);
 
-
+/**
+ * Here, we are defining a variable. 
+ * We use var to declare the varible "var1".
+ * We are assigning a string of text, denoted by the =""". 
+ * We are then outputting to the console. 
+ * CHECK: Functioning as intended.
+ */
 var var1 = "Value" 
 console.log(var1);
 
+/**
+ * This will output a blank line to the console.
+ */
 console.log();
 
+/**
+ * Here, we are defining the var2 with a number, and adding it with var1.
+ * CHECK: Functioning as intended.
+ */
 var var2 = 57;
 console.log(var1+var2);
 
 console.log(var1);
 
+/**
+ * Gives var3 a string of text, outputs to console.
+ * CHECK: Functioning as intended.
+ */
 var var3 = "Text"
 console.log(var3);
 
+/**
+ * Reassigns var3 to have a Boolean value, which is "true" in this case.
+ * CHECK: Functioning as intended. 
+ */
 var3 = true;
 console.log(var3);
 
+/**
+ * Reassigns var1 with the value of 5, outputs to console.
+ */
 var1 = 5;
 console.log(var1);
 
-var2 ="5"
+/**
+ * Reassigns varls with the value of 3, 
+ */
+var2 = "3"
 console.log(Number(var2));
 
 
