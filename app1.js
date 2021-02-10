da148c53e20a492c07747189f66698639cab2336
Hunter <hunter@Nest.local>
2021-02-10 17:10:10 -0500

    GM Final

---
+++ app1.js
@@ -0,0 +1,47 @@
+/**
+ * Console Methods
+ * Provides different handy features for debugging
+ * Reminds me of Includes? 
+ */
+console.warn("Uh oh, warning time!");
+console.error("Well, this error probably isn't good.");
+
+
+
+/**
+ * Here, we are defining a variable. 
+ * We use var to declare a variable, which is in this case, "greeting".
+ * Using console.log, we can display the instructions contained in our var, in the browser's debug console.
+ * NOTE: You can save work by remembering to declare and define the var at the same time. 
+ * REPORT: Functioning as intended. 
+ */
+
+var greeting = "Hello, Idiot."
+console.log(greeting);
+
+
+var var1 = "Value" 
+console.log(var1);
+
+console.log();
+
+var var2 = 57;
+console.log(var1+var2);
+
+console.log(var1);
+
+var var3 = "Text"
+console.log(var3);
+
+var3 = true;
+console.log(var3);
+
+var1 = 5;
+console.log(var1);
+
+var2 ="5"
+console.log(Number(var2));
+
+
+
+
